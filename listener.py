162e653e6e8c91a994581d59a65ecb484f030590
steve <meadsteve@gmail.com>
2018-09-06 13:55:53 +0200

    simple file lock for state

--- listener.py
+++ listener.py
@@ -1,6 +1,25 @@
-from kafka import KafkaConsumer
+import json
 
-consumer = KafkaConsumer('test', bootstrap_servers='172.17.0.1:9092')
+from kafka import KafkaConsumer, TopicPartition
+
+
+def decode(data):
+    return json.loads(data.decode("utf-8"))
+
+consumer = KafkaConsumer(bootstrap_servers='172.17.0.1:9092', value_deserializer=decode)
+partition = TopicPartition(topic='test', partition=0)
+consumer.assign([partition])
+
+try:
+    with open(".lockfile", 'r') as lockfile:
+        last_position = int(lockfile.readline())
+        print(f"resuming consumer at position {last_position}")
+        consumer.seek(partition, last_position + 1)
+except:
+    print("starting a new consumer from the latest position")
 
 for msg in consumer:
-    print (msg)
+    print(f"{msg.offset} - {msg.value}")
+    with open(".lockfile", 'w') as lockfile:
+        lockfile.seek(0)
+        lockfile.write(str(msg.offset))
--- publisher.py
+++ publisher.py
@@ -1,8 +1,18 @@
+import json
+import uuid
+
 from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='172.17.0.1:9092')
 
 TIMEOUT = 20
+RUN_ID = str(uuid.uuid4())
 
-results = [producer.send('test', b'hello').get(timeout=TIMEOUT) for i in range(10)]
+
+def encode(data):
+    return bytearray(json.dumps(data), "utf-8")
+
+for j in range(20):
+    requests = [producer.send('test', encode({"number": f"{j}-{i}", "run_id": RUN_ID})) for i in range(10)]
+    results = [request.get(timeout=TIMEOUT) for request in requests]
 
 print("done")
